583ffa10c08e858b1c46c153fc615a225032116b
Sarah <sjmadro1@gmail.com>
2019-05-01 18:00:16 -0700

    initialized firebase and set up in app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './Components/Common';
 
 class App extends Component {
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDM3EREbY2eu33Z2idTYJuNJ_E-dYkUV4o',
+      authDomain: 'authentication-db5a7.firebaseapp.com',
+      databaseURL: 'https://authentication-db5a7.firebaseio.com',
+      projectId: 'authentication-db5a7',
+      storageBucket: 'authentication-db5a7.appspot.com',
+      messagingSenderId: '581707649529',
+      appId: '1:581707649529:web:fec43efd2838e419'
+    });
+  }
+
   render() {
     return (
       <View>
